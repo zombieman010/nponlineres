90d2703eae3b7cb2b6fb95e87e8c29e2ed87ab0b
Nathaniel Pautzke <npautzke_10@hotmail.com>
2018-11-07 03:18:35 -0600

    Static pages with header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+  get '/projects', to: 'static_pages#projects'
+  get '/resume',   to: 'static_pages#resume'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
