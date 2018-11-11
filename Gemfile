ed34087ed83373beb86c4b14c5fcb7f12e470f32
Nathaniel Pautzke <npautzke_10@hotmail.com>
2018-11-11 00:47:35 -0600

    Adding Rspec and a few tests

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
+  gem 'rspec-rails', '>= 3.0.0'
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+
+  def setup
+    @base_title = "np.com"
+  end
+  test "should get root/home" do
+    get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get projects" do
-    get static_pages_projects_url
+    get projects_path
     assert_response :success
+    assert_select "title", "Projects | #{@base_title}"
   end
 
   test "should get resume" do
-    get static_pages_resume_url
+    get resume_path
     assert_response :success
+    assert_select "title", "Resume | #{@base_title}"
   end
 
 end
