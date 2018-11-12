56d7432725b5b51e8516f196d1fe481e79ea1748
Nathaniel Pautzke <npautzke_10@hotmail.com>
2018-11-11 18:03:14 -0600

    Routes mod.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get '/projects', to: 'static_pages#projects'
   get '/resume',   to: 'static_pages#resume'
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
